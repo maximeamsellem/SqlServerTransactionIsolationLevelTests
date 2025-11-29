1f05eb3ff01e53236b4c136231c0d82b6b1b8ac0
maxime amsellem <maxime.amsellem@gmail.com>
2025-11-29 14:57:09 +0100

    Initial commit to use test using MsSql with TestContainer

---
+++ SqlServer.Transaction.IsolationLevel.Tests/SqlServerContainerTests.cs
@@ -0,0 +1,41 @@
+﻿using Testcontainers.MsSql;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace SqlServer.Transaction.IsolationLevel.Tests;
+
+public class SqlServerContainerTests : IAsyncDisposable
+{
+    private readonly ITestOutputHelper _output;
+    private MsSqlContainer _sqlContainer;
+
+    public SqlServerContainerTests(ITestOutputHelper output) => _output = output;
+
+    public async ValueTask DisposeAsync()
+    {
+        if (_sqlContainer != null)
+        {
+            await _sqlContainer.StopAsync();
+        }
+    }
+
+    [Fact]
+    public async Task StartSqlServerContainer()
+    {
+        // Arrange
+        var sqlContainerBuilder = new MsSqlBuilder().WithPassword("Strong@Passw0rd");
+
+        _sqlContainer = sqlContainerBuilder.Build();
+
+        await _sqlContainer.StartAsync();
+
+        _output.WriteLine("SQL Server container started.");
+        _output.WriteLine($"Connection string : {_sqlContainer.GetConnectionString()}");
+
+        // Act
+        var connectionString = _sqlContainer.GetConnectionString();
+
+        // Assert
+        Assert.NotNull(connectionString);
+    }
+}
